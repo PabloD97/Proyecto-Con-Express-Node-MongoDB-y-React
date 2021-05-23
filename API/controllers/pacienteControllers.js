a04ed081d525d1547040d9e87efaa2c2d864ec01
PabloD97 <pablo97758@gmail.com>
2021-05-23 18:35:33 -0300

    feat: Router y Controller Paciente
    - Se añadio el put para actualizar pacientes con el endpoit '/pacientes/:id'
    - Se añadio la function en el controller para actualizar el paciente en base a su id

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,22 +1,22 @@
 const Paciente = require('../models/Paciente');
 
 // Cuando se crea un nuevo cliente
-exports.nuevoCliente = async (request, response, next)=> {
-    
+exports.nuevoCliente = async (request, response, next) => {
+
     // crear obj de paciente con datos de request.body
     const paciente = new Paciente(request.body);
 
     try {
         await paciente.save();
-        response.json({mensaje: 'El cliente se agrego correctamente'});
+        response.json({ mensaje: 'El cliente se agrego correctamente' });
 
     } catch (error) {
         console.log(error);
         next();
     }
-} 
+}
 
-exports.obtenerPacientes = async (request, response, next ) =>{
+exports.obtenerPacientes = async (request, response, next) => {
     try {
         const pacientes = await Paciente.find({});
         response.json(pacientes);
@@ -26,9 +26,23 @@ exports.obtenerPacientes = async (request, response, next ) =>{
     }
 }
 
-exports.obtenerPaciente = async (request, response, next) =>{
+exports.obtenerPaciente = async (request, response, next) => {
     try {
         const paciente = await Paciente.findById(request.params.id);
+        response.json(paciente);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+exports.actualizarPaciente = async (request, response, next) => {
+    try {
+        const paciente = await Paciente.findOneAndUpdate(
+            { _id : request.params.id },
+            request.body,
+            { new: true }
+        )
         response.json(paciente);
     } catch (error) {
         console.log(error);
--- API/routes/index.js
+++ API/routes/index.js
@@ -19,6 +19,10 @@ module.exports = function(){
         pacienteController.nuevoCliente
     )
 
+    // ACtualizar un registro con un ID especifico
+    router.put('/pacientes/:id', 
+        pacienteController.actualizarPaciente
+    )
 
     return router;
 }
