89aebde567e96a96d11e3067f3942613ec37056b
PabloD97 <pablo97758@gmail.com>
2021-05-23 18:05:41 -0300

    feat: Router
    - Se añadio el .get del endpoit '/pacientes' para obtener todos los registros

--- API/routes/index.js
+++ API/routes/index.js
@@ -4,9 +4,15 @@ const pacienteController = require('../controllers/pacienteControllers');
 
 module.exports = function(){ 
 
+    // Obtener todos los registros de pacientes en la db
+    router.get('/pacientes',
+        pacienteController.obtenerPacientes
+    )
+
+
     // Agrega nuevos pacientes via POST
     router.post('/pacientes', 
-        pacienteController.nuevoCliente()
+        pacienteController.nuevoCliente
     )
 
     return router;
