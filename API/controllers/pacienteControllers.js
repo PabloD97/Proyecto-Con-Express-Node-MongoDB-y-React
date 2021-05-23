fcd1e64e4640ba84c25b9f705371b76c712cd5c6
PabloD97 <pablo97758@gmail.com>
2021-05-23 17:55:23 -0300

    feat: Controller Paciente
    - Se agrego el codigo faltante para insertar un paciente a la DB

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,8 +1,17 @@
-const { response } = require("express")
-
+const Paciente = require('../models/Paciente');
 
 // Cuando se crea un nuevo cliente
-exports.nuevoCliente = (request, response, next)=> {
-    //TODO: Insertar en la db
-    response.json({mensaje: 'El cliente se agrego correctamente'});
-}
+exports.nuevoCliente = async (request, response, next)=> {
+    
+    // crear obj de paciente con datos de request.body
+    const paciente = new Paciente(request.body);
+
+    try {
+        await paciente.save();
+        response.json({mensaje: 'El cliente se agrego correctamente'});
+
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+} 
